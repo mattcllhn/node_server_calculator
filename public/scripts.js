139eac893a225bcee28efdff6bf368a89bc7a6c2
matt <mattcllhn@gmail.com>
2016-09-10 17:33:35 -0500

    working calculator with beginnings of other good stuff and whatnot

--- public/scripts.js
+++ public/scripts.js
@@ -2,10 +2,60 @@
 console.log('scripts.js sourced');
 $(document).ready(function(){
   console.log('jquery is working');
+// var calculator = $('<div />',{
+//     class:'calcBody',
+//     children:[$('<div />',{
+//       class:'btn-group btn-group-lg'
+//     })],
+//
+// });
+var number='';
+var equals=0;
+var ajaxObject={
+  aNumber:number
+};
+var displayObject={};
+var reset = function () {
+  $('#outputP').empty();
+  number='';
+};//reset
+
+  $('.btn').on('click',function(){
+    if(number!==''&&equals>0){
+    console.log('in the if, motherfucker');
+  }else{
+    number +=($(this).html());
+      $('#outputP').html(number);
+      ajaxObject.aNumber=number;
+    console.log(number);
+    console.log(ajaxObject);
+  }});//btn onclick
+
+  $('#calculate').on('click',function(){
+
+
+    $.ajax({
+      type:'POST',
+      url:'/results',
+      data: ajaxObject,
+      success: function(data){
+        console.log('this came back from the server',data);
+        reset();
+        displayObject={display:data.res};
+        $('#outputP').html(displayObject.display);
+      }//success
+    });//ajax
+  });//calculate onclick
+
+
+  $('#clear').on('click',function(){
+    reset();
+  });
 
 
 
 
 
-  
+
+
 });//docready
--- server/app.js
+++ server/app.js
@@ -9,8 +9,21 @@ app.listen('3000','localhost',function(){
   console.log('server is listening on port 3000');
   });//app.listen
 
-app.get('/', function(req,res){
+app.get('/', urlEncodedParser,function(req,res){
   console.log('Base url hit, yo');
   res.sendFile(path.resolve('public/index.html'));
 });//app.get
 app.use(express.static('public'));
+var objectoMundo={};
+app.post( '/results', urlEncodedParser,function ( req, res){
+  var objIn= req.body.aNumber;
+    console.log('--------------------------------------------------------------------->req:',objIn);
+  var number = eval(objIn);
+  console.log(number);
+objectoMundo={res:number};
+
+
+
+
+    res.send(objectoMundo);
+});
